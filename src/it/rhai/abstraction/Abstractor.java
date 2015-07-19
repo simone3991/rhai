f4e9d849fda903797b4837fe0993d75640ecdf1b
simone3991 <simone.colucci01@universitadipavia.it>
2015-07-19 15:21:48 +0200

    Refactoring

---
+++ src/it/rhai/abstraction/Abstractor.java
@@ -0,0 +1,29 @@
+package it.rhai.abstraction;
+
+import it.distanciable.Distanciable;
+import it.distanciable.sequences.Sequence;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * {@link Abstractor} interface represents any kind of object able to extract a
+ * {@link Sequence} from a source data file
+ * 
+ * @author simone
+ *
+ * @param <T>: the type of sequence to be extracted
+ */
+public interface Abstractor<T extends Distanciable<T>> {
+
+	/**
+	 * Extracts the sequence from the data file
+	 * 
+	 * @param data
+	 *            : the source data file
+	 * @return: the extracted sequence
+	 * @throws IOException 
+	 */
+	public Sequence<T> buildSequence(File data) throws IOException;
+
+}
---
+++ src/it/rhai/identification/Identifier.java
@@ -0,0 +1,47 @@
+package it.rhai.identification;
+
+import java.util.ArrayList;
+
+import it.distanciable.sequences.Sequence;
+import it.distanciable.sequences.SequenceRecognizer;
+import it.rhai.model.PowerConsumptionLabel;
+import it.rhai.settings.SettingsKeeper;
+import it.rhai.util.DataHandler;
+
+public class Identifier implements DataHandler<Sequence<PowerConsumptionLabel>> {
+
+	private SequenceRecognizer<PowerConsumptionLabel> recognizer;
+
+	public Identifier() {
+		this.recognizer = new SequenceRecognizer<PowerConsumptionLabel>(null);
+		recognizer.save(SettingsKeeper.getSettings().getLib());
+	}
+
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see it.rhai.reading.DataHandler#handle(java.lang.Object)
+	 */
+	public void handle(Sequence<PowerConsumptionLabel> data) {
+		Sequence<PowerConsumptionLabel> real = doIdentify(data).get(0);
+		String appliance = SettingsKeeper.getSettings().getAppliance(real);
+		SettingsKeeper.getSettings().getDebugLogger()
+				.println("active appliance: " + appliance);
+	}
+
+	private ArrayList<Sequence<PowerConsumptionLabel>> doIdentify(
+			Sequence<PowerConsumptionLabel> data) {
+		prepareRecognizer(data);
+		return recognizer.recognize(SettingsKeeper.getSettings()
+				.getMinimumLikelihood());
+	}
+
+	private void prepareRecognizer(Sequence<PowerConsumptionLabel> data) {
+		ArrayList<Sequence<PowerConsumptionLabel>> dataReceived = new ArrayList<Sequence<PowerConsumptionLabel>>(
+				1);
+		dataReceived.add(data);
+		recognizer.receiveMessage(dataReceived);
+	}
+
+}
